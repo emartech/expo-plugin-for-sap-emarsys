8b322b930d10b156a369c37554b8e8259041a4c9
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2026-01-27 16:15:58 +0100

    chore: add geofence mapper tests

--- example/plugins/ios/withNativeTest.ts
+++ example/plugins/ios/withNativeTest.ts
@@ -21,22 +21,23 @@ const withTestFiles = (config) => {
       if (!fs.existsSync(targetTestsDir)) {
         fs.mkdirSync(targetTestsDir, { recursive: true });
       }
-      const files = fs.readdirSync(sourceTestsDir);
-      for (const file of files) {
-        const sourcePath = path.join(sourceTestsDir, file);
-        const targetPath = path.join(targetTestsDir, file);
-        if (fs.statSync(sourcePath).isFile()) {
-          fs.copyFileSync(sourcePath, targetPath);
-        }
-      }
+
+      copyRecursive(sourceTestsDir, targetTestsDir);
 
       const podfilePath = path.join(projectRoot, 'ios', 'Podfile');
       if (fs.existsSync(podfilePath)) {
-        const podfileContent = fs.readFileSync(podfilePath, 'utf8');
+        let podfileContent = fs.readFileSync(podfilePath, 'utf8');
         
         if (!podfileContent.includes(`target '${TEST_TARGET_NAME}'`)) {
-          const testTarget = `\ntarget '${TEST_TARGET_NAME}' do\n  inherit! :search_paths\nend\n`;
-          fs.appendFileSync(podfilePath, testTarget);
+          const mainTargetRegex = /target\s+'expoemarsyspluginexample'\s+do([\s\S]*?)(\n\s*end)/;
+          const match = podfileContent.match(mainTargetRegex);
+          
+          if (match) {
+            const testTarget = `\n\n  target '${TEST_TARGET_NAME}' do\n    inherit! :complete\n  end`;
+            const replacement = `target 'expoemarsyspluginexample' do${match[1]}${testTarget}${match[2]}`;
+            podfileContent = podfileContent.replace(mainTargetRegex, replacement);
+            fs.writeFileSync(podfilePath, podfileContent, 'utf8');
+          }
         }
       }
 
@@ -53,13 +54,15 @@ const withTestTarget = (config) => {
 
     const projectRoot = config.modRequest.projectRoot;
     const targetTestsDir = path.join(projectRoot, 'ios', TEST_TARGET_NAME);
-    const testFiles = fs.existsSync(targetTestsDir) 
-      ? fs.readdirSync(targetTestsDir).filter(file => fs.statSync(path.join(targetTestsDir, file)).isFile())
-      : [];
+    const testFiles = collectFiles(targetTestsDir);
 
     const objects = config.modResults.hash.project.objects;
     objects['PBXTargetDependency'] = objects['PBXTargetDependency'] || {};
     objects['PBXContainerItemProxy'] = objects['PBXContainerItemProxy'] || {};
+
+    const mainTargetKey = config.modResults.getFirstTarget().uuid;
+    const mainTarget = objects['PBXNativeTarget'][mainTargetKey];
+    const originalDependencies = mainTarget.dependencies ? [...mainTarget.dependencies] : [];
 
     const target = config.modResults.addTarget(
       TEST_TARGET_NAME,
@@ -68,11 +71,27 @@ const withTestTarget = (config) => {
       `${config.ios?.bundleIdentifier}.${TEST_TARGET_NAME}`,
     );
 
+    // Restore original dependencies
+    mainTarget.dependencies = originalDependencies;
+
+    const filesByDir = {};
+    for (const file of testFiles) {
+      const dir = path.dirname(file);
+      if (!filesByDir[dir]) {
+        filesByDir[dir] = [];
+      }
+      filesByDir[dir].push(path.basename(file));
+    }
+    
+    const rootFiles = filesByDir['.'] || [];
+    const subdirs = Object.keys(filesByDir).filter(d => d !== '.');
+
     const pbxGroup = config.modResults.addPbxGroup(
-      testFiles,
+      rootFiles,
       TEST_TARGET_NAME,
       TEST_TARGET_NAME,
     );
+
     const groups = config.modResults.hash.project.objects['PBXGroup'];
     for (const key of Object.keys(groups)) {
       if (typeof groups[key] === 'object' && groups[key].name === undefined && groups[key].path === undefined) {
@@ -80,10 +99,36 @@ const withTestTarget = (config) => {
       }
     }
 
+    const createdGroups = {};
+    for (const dir of subdirs) {
+      const files = filesByDir[dir];
+      const dirParts = dir.split(path.sep);
+      const dirName = dirParts[dirParts.length - 1];
+      
+      const subGroup = config.modResults.addPbxGroup(
+        files,
+        dirName,
+        dirName
+      );
+      
+      createdGroups[dir] = subGroup.uuid;
+      
+      if (dirParts.length === 1) {
+        config.modResults.addToPbxGroup(subGroup.uuid, pbxGroup.uuid);
+      } else {
+        const parentDir = dirParts.slice(0, -1).join(path.sep);
+        const parentGroupId = createdGroups[parentDir];
+        if (parentGroupId) {
+          config.modResults.addToPbxGroup(subGroup.uuid, parentGroupId);
+        }
+      }
+    }
+
     const sourceFiles = testFiles.filter(file => file.endsWith('.m'));
     if (sourceFiles.length > 0) {
+      const sourceFileNames = sourceFiles.map(f => path.basename(f));
       config.modResults.addBuildPhase(
-        sourceFiles,
+        sourceFileNames,
         'PBXSourcesBuildPhase',
         'Sources',
         target.uuid
@@ -115,6 +160,8 @@ const withTestTarget = (config) => {
           }
           buildSettings['INFOPLIST_FILE'] = `${TEST_TARGET_NAME}/Test-Info.plist`;
           buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1';
+          buildSettings['TEST_HOST'] = '"$(BUILT_PRODUCTS_DIR)/expoemarsyspluginexample.app/$(BUNDLE_EXECUTABLE_FOLDER_PATH)/expoemarsyspluginexample"';
+          buildSettings['BUNDLE_LOADER'] = '"$(TEST_HOST)"';
         }
       }
     }
@@ -122,6 +169,35 @@ const withTestTarget = (config) => {
     return config;
   });
 }
+
+const copyRecursive = (src, dest) => {
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      if (!fs.existsSync(destPath)) fs.mkdirSync(destPath, { recursive: true });
+      copyRecursive(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+};
+
+const collectFiles = (dir, baseDir = dir) => {
+  const results = [];
+  if (!fs.existsSync(dir)) return results;
+
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry.name);
+    if (entry.isDirectory()) results.push(...collectFiles(fullPath, baseDir));
+    else results.push(path.relative(baseDir, fullPath));
+  }
+  return results;
+};
+
 
 module.exports = (config) => {
   config = withTestFiles(config);
